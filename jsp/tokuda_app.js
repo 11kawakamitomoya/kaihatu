4194be51cfb7add6da2a20eba0c2d8fe932f7fc5
23110011 <23110011@oic-ok.ac.jp>
2024-06-05 10:46:41 +0900

    html完成 ver1.0

---
+++ jsp/tokuda_app.js
@@ -0,0 +1,6 @@
+function changeColor1() {
+    document.getElementById('haikeiColor').style.backgroundColor = "#e0ffff";
+}
+function changeColor2() {
+    document.getElementById('haikeiColor').style.backgroundColor = "#ffffe0";
+}
