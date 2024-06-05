5f88c18598dd7dab2c8d53b64b619af3f8a6d9a3
23110011 <23110011@oic-ok.ac.jp>
2024-06-05 11:18:49 +0900

    js ver1.0

--- jsp/tokuda_app.js
+++ jsp/tokuda_app.js
@@ -1,5 +1,5 @@
 function changeColor1() {
-    document.getElementById('haikeiColor').style.backgroundColor = "#e0ffff";
+    document.getElementById('haikeiColor').style.backgroundColor = "#ffffDD";
 }
 function changeColor2() {
     document.getElementById('haikeiColor').style.backgroundColor = "#ffffe0";
